51f994f2b461ca202f7d50a72b86e85e13bbff5d
Luis <lmadero@ari.group>
2023-04-10 12:23:20 -0600

    act

---
+++ Boton.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Boton extends JFrame implements ActionListener{
+    JButton boton1;
+    public Boton(){
+        setLayout(null);
+        boton1 = new JButton("Cerrar");
+        boton1.setBounds(300,250,100,30);
+        add(boton1);
+        //se le agrega un evento
+        boton1.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==boton1){
+          System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        Boton formulario1 = new Boton();
+        formulario1.setBounds(0,0,450,350);
+        formulario1.setVisible(true);
+        formulario1.setResizable(false);
+        formulario1.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ Form.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Form extends JFrame{
+    public Form(){
+        setLayout(null);
+    }
+
+    public static void main(String[] args) {
+        Form formulario1 =  new Form();
+        formulario1.setBounds(0,0,400,550);
+        formulario1.setVisible(true);
+        formulario1.setLocationRelativeTo(null);
+	    //este es para que el tamaño sea fijo
+        formulario1.setResizable(false);
+    }
+}
